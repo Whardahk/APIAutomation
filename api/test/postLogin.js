05dfdf9834972e893bcb38431f53291dfbf406e8
Whardah <whardah25@gmail.com>
2022-11-21 21:36:45 +0700

    post login with valid username & password

---
+++ api/test/postLogin.js
@@ -0,0 +1,20 @@
+const { baseUrl } = require("../data/baseUrl");
+const request = require("supertest")(baseUrl);
+const expect = require("chai").expect;
+const { USER_DATA } = require("../data/userData");
+
+const endpoint = "/api/authaccount/login";
+
+describe(`POST ${endpoint}`, function () {
+  it("Login with valid username & password", async function () {
+    const response = await request.post(endpoint).send({
+      email: USER_DATA.email,
+      password: USER_DATA.password,
+    });
+
+    expect(response.status).to.eql(200);
+    expect(response.body.code).to.eql(0);
+    expect(response.body.data.Name).to.eql(USER_DATA.name);
+    expect(response.body.data.Email).to.eql(USER_DATA.email);
+  });
+});
